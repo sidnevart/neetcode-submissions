38ca1efdf5cdd9c26de492dd459e70a71668a9e9
Artem Sidnev <a.sidnevart@gmail.com>
2026-09-14 01:05:23 +0400

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.go
@@ -0,0 +1,45 @@
+func isValidSudoku(board [][]byte) bool {
+	for i := 0; i < len(board); i++ {
+		seenInRow := make(map[byte]bool)
+		seenInCol := make(map[byte]bool)
+
+		for j := 0; j < len(board); j++ {
+			rowValue := board[i][j]
+			colValue := board[j][i]
+
+			if rowValue != '.' {
+				if seenInRow[rowValue] {
+					return false
+				}
+
+				seenInRow[rowValue] = true
+			}
+
+			if colValue != '.' {
+				if seenInCol[colValue] {
+					return false
+				}
+
+				seenInCol[colValue] = true
+			}
+		}
+	}
+
+	// boxes
+	for boxRow := 0; boxRow < 3; boxRow++ {
+		for boxCol := 0; boxCol < 3; boxCol++ {
+			seenInBox := make(map[byte]bool)
+			for r := boxRow * 3; r < boxRow * 3 + 3; r++{
+				for c := boxCol * 3; c < boxCol * 3 + 3; c++{
+					if board[r][c] != '.' {
+						if seenInBox[board[r][c]] {
+							return false
+						}
+						seenInBox[board[r][c]] = true
+					}
+				}
+			}
+		}
+	}
+	return true
+}
